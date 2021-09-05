07c5a348b0cdacefffb02876cf80450514368c92
topsy404 <asl053044@gmail.com>
2021-09-06 00:01:58 +0800

    add normalizer

--- py_meshgraphnet/normalization.py
+++ py_meshgraphnet/normalization.py
@@ -13,11 +13,46 @@ class Normalizer(torch.nn.Module):
                  std_epsilon=1e-8,
                  name="Normalizer"
                  ):
-        super(Normalizer, self).__init__()
+        super(Normalizer, self).__init__(name = name)
         self._max_accumulations = max_accumulations
         self._std_epsilon = std_epsilon
+        self._acc_count = torch.tensor(0)
+        self._num_accumulations = torch.tensor(0)
+        self._acc_sum = torch.zeros(size)
+        self._acc_sum_squared = torch.zeros(size)
+
 
     def forward(self, batched_data, accumulate = True):
-        return None
+        if accumulate:
+            if self._num_accumulations < self._max_accumulations:
+                self._accumulate(batched_data)
+        return (batched_data - self._mean()) / self._std_with_epsilon()
+
+    def inverse(self, normalized_batch_data):
+        return normalized_batch_data * self._std_with_epsilon() + self._mean()
+
+    def _accumulate(self, batched_data):
+        """
+        shape ?, 7
+        :param batched_data:
+        :return:
+        """
+        count = torch.tensor(batched_data.shape[0], dtype=torch.float32)
+        data_sum = torch.sum(batched_data, dim = 0)
+        squared_data_sum = torch.sum(batched_data ** 2, dim = 0)
+        self._acc_sum += data_sum
+        self._acc_sum_squared += squared_data_sum
+        self._acc_count += count
+        self._num_accumulations += 1
+
+    def _mean(self):
+        safe_count = torch.maximum(self._acc_count, 1.0)
+        return self._acc_sum / safe_count
+
+    def _std_with_epsilon(self):
+        safe_count = torch.maximum(self._acc_count, 1.0)
+        std = torch.sqrt(self._acc_sum_squared / safe_count - self._mean()  ** 2)
+        return torch.maximum(std, self._std_epsilon)
+
 
 
---
+++ py_meshgraphnet/test_model.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: topsy 
+# Data: 2021/9/5 下午3:46
